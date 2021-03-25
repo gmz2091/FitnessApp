73115af10e8208cd3f7f73e867d404717d90086b
magdielG <mcdielgmz@hotmail.com>
2021-03-25 13:42:51 -0700

    Add config devServer

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "root"),
+    filename: "[name].[contenthash].js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: path.resolve(__dirname, "assets/[name].[contenthash].css"),
+    }),
+  ],
+  devServer: {
+    compress: true,
+    host: "192.168.1.130",
+    port: "8003",
+  },
+};

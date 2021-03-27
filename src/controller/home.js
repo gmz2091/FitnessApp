a698bcca84d1681dd9984b2a73be48e702c57e7a
magdielG <mcdielgmz@hotmail.com>
2021-03-26 17:36:42 -0700

    modified:   root/index.html 	deleted:    root/main.eaa7be03ccb3309cbd5d.js 	modified:   src/controller/home.js 	modified:   src/view/home.html 	root/main.9cd1e919d4dfa7ef787f.js

--- src/controller/home.js
+++ src/controller/home.js
@@ -5,23 +5,36 @@ import getData from "../utils/getData";
 const Home = () => {
   const divElement = document.createElement("div");
 
-  divElement.classList = "container";
+  divElement.classList = "mx-4 xs:mx-4";
   divElement.innerHTML = view;
 
   const searchFood = divElement.querySelector("#searchFood");
 
   searchFood.addEventListener("click", async (e) => {
     let inputNamas = divElement.querySelector("#name");
-    
 
     e.preventDefault();
     if (inputNamas.value === "") {
       console.error("El Campo esta vacio");
       return;
     }
-    const foodName = inputNamas.value.replaceAll(" ", "%20")
+    const foodName = inputNamas.value.replaceAll(" ", "%20");
+    const fooData = divElement.querySelector("#dataFood");
     const data = await getData(foodName);
-    console.log(data);
+    const view = `
+     ${data
+       .map(
+         (element) => `
+     <div class="bg-green-900">
+         <p class="text-2xl not-italic">${element.description}</p>
+         ${element.foodNutrients.map((elements) => `
+         <p class="text-2xl not-italic text-indigo-300	">${elements.name}:<span>${elements.number}</span>gr</p>`)}
+     </div>`
+       )
+       .join("")}`;
+    fooData.innerHTML = view;
+    divElement.appendChild(fooData);
+    console.log(fooData);
     inputNamas.value = "";
   });
 
